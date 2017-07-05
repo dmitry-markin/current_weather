36f4d4e21068d34045444131536130eb3841a486
Dmitry Markin <dmitry@markin.tech>
2017-07-05 18:59:19 +0300

    Implement settings activity and fragment

---
+++ app/src/main/java/tech/markin/currentweather/SettingsActivity.java
@@ -0,0 +1,21 @@
+package tech.markin.currentweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Dmitry on 05.07.2017.
+ */
+
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
---
+++ app/src/main/java/tech/markin/currentweather/SettingsFragment.java
@@ -0,0 +1,64 @@
+package tech.markin.currentweather;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.ListPreference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Dmitry on 05.07.2017.
+ */
+
+public class SettingsFragment extends PreferenceFragment
+                              implements SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final String KEY_PREF_LOCATION = "pref_location";
+    public static final String KEY_PREF_INTERVAL = "pref_interval";
+    public static final String KEY_PREF_SHOW_LOCKED = "pref_show_locked";
+    public static final String KEY_PREF_SHOW_UNLOCKED = "pref_show_unlocked";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        // Set location summary
+        EditTextPreference prefLocation = (EditTextPreference)findPreference(KEY_PREF_LOCATION);
+        prefLocation.setSummary(prefLocation.getText());
+
+        // Set update interval summary
+        ListPreference prefInterval = (ListPreference)findPreference(KEY_PREF_INTERVAL);
+        prefInterval.setSummary(prefInterval.getEntry());
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(KEY_PREF_LOCATION)) {
+            Preference prefLocation = findPreference(key);
+            prefLocation.setSummary(sharedPreferences.getString(key, ""));
+
+        } else if (key.equals(KEY_PREF_INTERVAL)) {
+            ListPreference prefInterval = (ListPreference)findPreference(key);
+            int index = prefInterval.findIndexOfValue(sharedPreferences.getString(key, ""));
+            prefInterval.setSummary(index >= 0
+                                        ? prefInterval.getEntries()[index]
+                                        : null);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceManager().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
