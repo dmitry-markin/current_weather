1296a99488b3da8ad1eef9fcb83eb26b3820e8e0
Dmitry Markin <dmitry@markin.tech>
2017-07-06 11:39:45 +0300

    Show dummy notification

--- app/src/main/java/tech/markin/currentweather/UpdateWeatherReceiver.java
+++ app/src/main/java/tech/markin/currentweather/UpdateWeatherReceiver.java
@@ -1,14 +1,26 @@
 package tech.markin.currentweather;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
 
 public class UpdateWeatherReceiver extends BroadcastReceiver {
+    private static final int NOTIFICATION_ID = 1;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Updating current weather...", Toast.LENGTH_SHORT).show();
+        Notification notification  = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Weather")
+                .setContentText("Hello world!")
+                .build();
+        notification.flags |= Notification.FLAG_NO_CLEAR;
+
+        NotificationManager notificationMgr =
+                (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationMgr.notify(NOTIFICATION_ID, notification);
     }
 }
